40118b26cc778a412ef933e8eef249c9c24927a6
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2021-01-15 00:15:55 -0600

    Stubbed out edit route.

--- routes/editRoutes.js
+++ routes/editRoutes.js
@@ -7,7 +7,13 @@ const { baseURL, dbUpdate } = require('../globals');
 const { Playlist, Track } = require('../models');
 
 router.get('/playlist/:id/edit', ensureAuthenticated, async (req, res) => {
-    res.send('Editing playlist: ' + req.params.id);
+    const playlistQuery = await Playlist.select(req.params.id);
+    console.log(playlistQuery);
+    playlist = playlistQuery.rows[0];
+    const trackQuery = await Track.select(playlist.track_ids);
+    let tracks = trackQuery.rows;
+
+    res.render('playlistEdit', { user: req.user, name: playlist.name, tracks: tracks });
 });
 
-module.exports = router;
+module.exports = router; 
